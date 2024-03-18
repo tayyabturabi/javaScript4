c36eeca42b01d78adedf961b9ce867ad9e601045
Muhammad Tayyab <786.mtayyab@gmail.com>
2024-03-18 21:01:36 +0500

    completed

---
+++ app.js
@@ -0,0 +1,209 @@
+//=============================>  Assigment#4  <=============================//
+
+// //Question# 1
+// var studentName = [];
+
+
+// //Question# 2
+// var studentNames = {
+//     names: []
+// };
+
+// //Question# 3
+// var studentName = ["ali","kassim","areeb","anas"];
+
+// //Question# 4
+// var studentName = [23,35,36,37];
+
+// //Question# 5
+// var myCondition = [true];
+
+// //Question# 6
+// var myArr = ["Ali", true , 34 , null, undefined];
+
+
+
+// Question # 7
+
+
+// var arr = ["SSC","HSC","BCS","BCom","BS","MS","MPhil","PHD"]
+// document.write(`
+// <h1> Degrees Name </h1>
+// <ol>
+// <li>) ${arr[0]} <br> </li>
+// <li>) ${arr[2]} <br> </li>
+// <li>) ${arr[3]} <br> </li>
+// <li>) ${arr[4]} <br> </li>
+// <li>) ${arr[5]} <br> </li>
+// <li>) ${arr[6]} <br> </li>
+// <li>) ${arr[7]} <br> </li>
+// </ol>
+
+// `)
+
+
+//Question # 8 
+
+// var studentName = ["Michael","Jhon","Tony"];
+// var score = [320,230,480]
+// var ttScore = [500]
+
+// document.write (`
+// Score of Michael is ${score[0]}. Percentage:${score[0]/ttScore*100} %  <br>
+// Score of Jhon is ${score[1]}. Percentage:${score[1]/ttScore*100} %  <br>
+// Score of Tony is ${score[2]}. Percentage:${score[2]/ttScore*100} %  <br>
+
+// `)
+
+
+
+// Question # 9
+
+// var colorList = ["Red","Green","Blue","Pink","Orange"];
+// document.write(colorList);
+
+// Question # 9(a)
+// var colorList = ["Red","Green","Blue","Pink","Orange"];
+// colorList.unshift("See Green") 
+// document.write (`${colorList}`);
+
+// Question # 9(b)
+// var colorList = ["Red","Green","Blue","Pink","Orange"];
+// colorList.push("Indigo") 
+// document.write (`${colorList}`);
+
+// Question # 9(c)
+// var colorList = ["Red","Green","Blue","Pink","Orange"];
+// colorList.unshift("Indigo","Sea Green"); 
+// document.write (`${colorList}`);
+
+
+// Question # 9(d)
+// var colorList = ["Red","Green","Blue","Pink","Orange"];
+// colorList.shift(); 
+// document.write (`${colorList}`);
+
+// Question # 9(e)
+// var colorList = ["Red","Green","Blue","Pink","Orange"];
+// colorList.pop(); 
+// document.write (`${colorList}`);
+
+
+
+// Question # 9(f)
+// var colorList = ["Red","Green","Blue","Pink","Orange"];
+// colorList.splice(2,0,"Yellow"); 
+// document.write (`${colorList}`);
+
+// Question # 9(g)
+// var colorList = ["Red","Green","Blue","Pink","Orange"];
+// colorList.splice(2,2); 
+// document.write (`${colorList}`);
+
+
+
+
+
+//Question # 10 
+
+// var studentScore = [320,230,480,120];
+// document.write(`
+// Score of Students : ${studentScore} <br>
+// Order Score of Stundents : ${studentScore.sort()}
+// `)
+
+
+//Question # 11 
+
+// var citiesList = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+// document.write(`
+// Cities List : <br>  ${citiesList} <br><br><br>
+
+// Selected Cities List : <br> ${citiesList.slice(2,4)}
+
+// `)
+
+// Question # 12
+
+// var arr1 = ["This","is","my","cat"];
+// arr1.join()
+// document.write(`
+
+// <h1> String </h1>
+// <h2> This,is,my,cat </h2>
+
+// <h1> Array </h1>
+// <h2> ${arr1[0]+" "+arr1[1]+" "+arr1[2]+" "+arr1[3]} </h2>
+// `);
+
+
+//Question # 13
+//  var arr = ["Key Board","Mouse","Printer","Monitor"]
+//  document.write(`
+ 
+//  <h1> Devices </h1>
+//  <h3>${arr} </h3>
+ 
+//  <h2>Out:</h2>
+//  <h3>${arr[0]}</h3> <br>
+
+//  <h2>Out:</h2>
+//  <h3>${arr[1]}</h3> <br>
+
+//  <h2>Out:</h2>
+//  <h3>${arr[2]}</h3> <br>
+
+//  <h2>Out:</h2>
+//  <h3>${arr[3]}</h3> <br>
+ 
+ 
+//  `);
+
+
+//Question # 14
+//  var arr = ["Key Board","Mouse","Printer","Monitor"]
+//  document.write(`
+ 
+//  <h1> Devices </h1>
+//  <h3>${arr} </h3>
+ 
+//  <h2>Out:</h2>
+//  <h3>${arr[3]}</h3> <br>
+
+//  <h2>Out:</h2>
+//  <h3>${arr[2]}</h3> <br>
+
+//  <h2>Out:</h2>
+//  <h3>${arr[1]}</h3> <br>
+
+//  <h2>Out:</h2>
+//  <h3>${arr[0]}</h3> <br>
+ 
+ 
+//  `);
+
+
+//Question # 15
+//   var arr = ["Apple","Samsung","Motorola","Nokia","Sony","Heier"]
+//   document.write(`
+
+//   <select>
+//   <option>Select</Option>
+//   <option>${arr[0]}</Option>
+//   <option>${arr[1]}</Option>
+//   <option>${arr[2]}</Option>
+//   <option>${arr[3]}</Option>
+//   <option>${arr[4]}</Option>
+//   <option>${arr[5]}</Option>
+// </select>
+  
+  
+//   `)
+
+
+
+
+
+
+
+
